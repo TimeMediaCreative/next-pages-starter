9a601b46022aeb2edf22e238e039a6410bbb9804
Mich CNC <michochieng@gmail.com>
2024-03-14 11:35:58 -0700

    Added fetch for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import { SanityDocument } from "next-sanity";
 
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      Populate me with Sanity Content
-    </main>
-  );
+import { getClient } from "@/sanity/lib/client";
+import { token } from "@/sanity/lib/token";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+import Posts from "@/components/Posts";
+
+type PageProps = {
+  posts: SanityDocument[];
+  draftMode: boolean;
+  token: string;
+};
+
+export default function Home(props: PageProps) {
+  return <Posts posts={props.posts} />;
 }
+
+export const getStaticProps = async ({ draftMode = false }) => {
+  const client = getClient(draftMode ? token : undefined);
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
+
+  return {
+    props: {
+      posts,
+      draftMode,
+      token: draftMode ? token : "",
+    },
+  };
+};
